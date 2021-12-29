b7de2c574a403c95d4d1119fc33e896f7c2242bb
sweera <sweerachahal95@gmail.com>
2021-12-28 20:58:28 -0800

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     devtool: 'source-map',
@@ -12,5 +12,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
